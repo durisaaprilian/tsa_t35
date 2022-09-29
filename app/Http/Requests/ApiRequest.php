4476003481435b4991183dd6c885aad7dc8edd50
durisaaprilian <mohammaddurisa@gmail.com>
2022-09-29 18:39:11 +0700

    first commit

---
+++ app/Http/Requests/ApiRequest.php
@@ -0,0 +1,50 @@
+<?php
+
+namespace App\Http\Requests;
+
+use Illuminate\Foundation\Http\FormRequest;
+
+abstract class ApiRequest extends FormRequest
+{
+    use ApiResponse;
+
+    abstract public function rules();
+
+    protected function failedValidation(Validator $validator)
+    {
+        throw new HttpResponseException($this->apiError(
+            $validator->errors(),
+            Response::HTTP_UNPROCESSABLE_ENTITY,
+        ));
+    }
+
+    protected function failedAuthorization()
+    {
+        throw new HttpResponseException($this->apiError(
+            null,
+            Response::HTTP_UNAUTHORIZED
+        ));
+    }
+
+    // /**
+    //  * Determine if the user is authorized to make this request.
+    //  *
+    //  * @return bool
+    //  */
+    // public function authorize()
+    // {
+    //     return false;
+    // }
+
+    // /**
+    //  * Get the validation rules that apply to the request.
+    //  *
+    //  * @return array<string, mixed>
+    //  */
+    // public function rules()
+    // {
+    //     return [
+    //         //
+    //     ];
+    // }
+}
